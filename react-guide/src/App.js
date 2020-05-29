f1654a8d36c70081687a4eca3f90123cb4b0ba0a
AbdullatifJamous891 <Abdullatif.jamous891@gmail.com>
2020-05-29 04:12:37 +0300

    props children

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -7,7 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hello I am React App</h1>
-        <Person name="Abdullatif" age="30" />
+        <Person name="Abdullatif" age="30">
+          {" "}
+          and I am a software developer{" "}
+        </Person>
         <Person name="Omer" age="25" />
         <Person name="Emad Eddin" age="20" />
       </div>
