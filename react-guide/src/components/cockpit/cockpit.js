797469f4bfd33a64bcda1f6ab67e454e44e66a64
abdullatifJamous891 <Abdullatif.jamous891@gmail.com>
2020-06-11 13:35:31 +0300

    useEffect

--- react-guide/src/components/cockpit/cockpit.js
+++ react-guide/src/components/cockpit/cockpit.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from "./cockpit.css";
 
 const cockpit = props => {
+  useEffect(() => {
+    console.log("[cockpit.js] useEffect");
+    setTimeout(() => {
+      alert("Saved data to cloud!");
+    }, 1000);
+  }, [props.persons]);
   let btnClass = "";
   if (props.showPersons) {
     btnClass = classes.Red;
