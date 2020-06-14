baf1b2f5165cc7e2b3e9453bd20c62329513e455
abdullatifJamous891 <Abdullatif.jamous891@gmail.com>
2020-06-14 10:06:05 +0300

    aux component

--- react-guide/src/components/Persons/Person/Person.js
+++ react-guide/src/components/Persons/Person/Person.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 import classes from "./Person.css";
+import Aux from "../../../hoc/aux";
 
 class Person extends Component {
   render() {
     console.log("[person.js] rendering...");
     return (
-      <div className={classes.Person}>
+      <Aux className={classes.Person}>
         <p onClick={this.props.click}>
           I am {this.props.name} and I am {this.props.age} years old!
         </p>
         <input type="text" onChange={this.props.changed} />
         <p>{this.props.children}</p>
-      </div>
+      </Aux>
     );
   }
 }
